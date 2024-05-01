9062bd1fc887ff781c86bb510f312da4a1ef45de
Emanuele <egentili2000@gmail.com>
2024-05-01 09:49:21 +0200

    Revert

--- services/services.go
+++ services/services.go
@@ -30,8 +30,6 @@ type Result int
 type Updated map[string]string
 type Done bool
 
-var count map[string]int
-
 func (state *LoadBalancer) ServeRequest(args Args, result *Result) error {
 	//Format service string
 	service := fmt.Sprintf("Server.%s", args.Service)
@@ -61,9 +59,7 @@ func (state *LoadBalancer) ServeRequest(args Args, result *Result) error {
 
 func (state *LoadBalancer) sendRequestToOneServer(service string, args Args, result *Result) error {
 	serverName := state.chooseFirstServer()
-	count[serverName]++
 	state.NumberOfPending[serverName]++
-	printState()
 	server := state.connect(serverName)
 	fmt.Printf("Send request to %s \n", serverName)
 	start := time.Now()
@@ -86,12 +82,9 @@ func (state *LoadBalancer) sendRequestToOneServer(service string, args Args, res
 func (state *LoadBalancer) sendRequestToTwoServer(service string, args Args, result *Result) (error, error) {
 	//Choose the two server and count the new request
 	serverName1 := state.chooseFirstServer()
-	count[serverName1]++
 	state.NumberOfPending[serverName1]++
 	serverName2 := state.chooseSecondServer(serverName1)
-	count[serverName2]++
 	state.NumberOfPending[serverName2]++
-	printState()
 	fmt.Printf("Send request to %s and %s \n", serverName1, serverName2)
 	server1 := state.connect(serverName1)
 	server2 := state.connect(serverName2)
@@ -334,10 +327,4 @@ func (state *LoadBalancer) connect(serverName string) *rpc.Client {
 		delete(state.ChoiceProbability, serverName)
 	}
 	return server
-}
-
-func printState() {
-	for s, i := range count {
-		print("%s : %d \n", s, i)
-	}
 }
